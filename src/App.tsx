db36957aaef3fa87133a5b17821aadae1ab7e3bc
Beat Hagenlocher <bah@posteo.de>
2026-02-15 18:45:26 +0100

    Add signout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { AudioPlayer } from './components/AudioPlayer'
 import { ImperativeAudioPlayer } from './components/ImperativeAudioPlayer'
 import { SignUpForm } from './components/SignUpForm'
 import { createModal } from './components/Modal'
+import { authClient } from './auth-client'
 
 const App = () => {
   const { openModal, Modal } = createModal()
+  const session = authClient.useSession()
+
+  const signOut = async () => {
+    const resp = await authClient.signOut()
+    console.log(resp)
+  }
 
   return (
     <>
@@ -14,6 +21,13 @@ const App = () => {
       >
         Sign Up
       </button>
+      <button
+        class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg"
+        onClick={signOut}
+      >
+        Sign Out
+      </button>
+      <pre>{JSON.stringify(session().data?.user, null, 2)}</pre>
       <Modal>
         <SignUpForm />
       </Modal>
