420b6fe95fec0874eb470070b8ede78f1d3e32e2
Beat Hagenlocher <bah@posteo.de>
2026-02-16 00:22:47 +0100

    Fix import breakage

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,5 +1,8 @@
 import { inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/solid'
-import { auth } from '../auth'
 
-export const authClient = createAuthClient({ plugins: [inferAdditionalFields<typeof auth>()] })
+export const authClient = createAuthClient({
+  plugins: [
+    inferAdditionalFields({ user: { automergeRootId: { type: 'string', required: true } } }),
+  ],
+})
