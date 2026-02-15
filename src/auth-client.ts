05e62b29503e86ad51a9c284f915a25fb951d9d5
Beat Hagenlocher <bah@posteo.de>
2026-02-15 18:50:52 +0100

    Correctly infer type

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,3 +1,5 @@
+import { inferAdditionalFields } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/solid'
+import { auth } from '../auth'
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({ plugins: [inferAdditionalFields<typeof auth>()] })
